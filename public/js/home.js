b856017c082b6b0053388cde658e9d52ee8ee716
Romain <romain.sire@gmx.com>
2020-07-15 15:25:39 +0200

    Optimisation du fichier home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,25 +1,26 @@
+/**
+ * Affichage de toutes les caméras disponibles
+ */
+const targetDiv = document.querySelector("#products-list > div");
 // Récupération des données
 const request = new Request();
 request.getJson("/api/cameras")
     .then(cameras => {
-        const targetDiv = document.querySelector("#products-list > div");
         // Pour chaque caméra récupérée, on crée une carte html, et on l'ajoute à la div cible :
-        for (const camera of cameras) {
+        cameras.reduce((acc, camera) => {
             const build = new BuildHtml();
             const card = build.homeCard(camera);
             targetDiv.appendChild(card);
-        }
+        }, "");
     })
     .catch(error => {
-        console.error("Erreur : " + error);
         // Ajout du message d'erreur sur la page :
         const build = new BuildHtml();
         const errorDiv = build.errorMessage("Un petit problème est survenu...");
-        const targetDiv = document.querySelector("#products-list > div");
         targetDiv.appendChild(errorDiv);
     });
-
-
-// Afficher le nombre de produits du panier dans le header
+/**
+ * Afficher le nombre de produits du panier dans le header
+ */
 const cart = new Cart();
 cart.displayNumberOfProductsInHeader();
